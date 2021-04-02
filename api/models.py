9798665e9ef484b4408d560c1df72c5085b5f620
Abhijeet Singh Songara <abhijeetss213@gmail.com>
2021-04-02 20:00:23 +0530

    training projectm

--- api/models.py
+++ api/models.py
@@ -3,7 +3,12 @@ from django.db import models
 # Create your models here.
 class Stu(models.Model):
     name = models.CharField(max_length=20)
-    address = models.CharField(max_length=40)
+    #address = models.CharField(max_length=40)
     age = models.IntegerField()
     totalmarks = models.IntegerField()
+
+class State(models.Model):
+    user = models.ForeignKey(Stu,on_delete = models.CASCADE , related_name='addr_set')
+    state = models.CharField(max_length=30)
+
 
